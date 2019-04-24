bd7fc3b17deea3906faa4b83b431def80b5e7fb1
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2019-04-23 20:11:07 -0400

    responsive pass 1

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('body')
-    <div class="min-h-screen bg-repeat" style="background-image: url('/assets/images/seamless_paper_texture.png');">
+    <div class="min-h-screen w-full bg-repeat" style="background-image: url('/assets/images/seamless_paper_texture.png');">
         <div class="container mx-auto">
             <div class="flex text-center items-center justify-center pt-12 mb-6">
                 <span class="font-hobbiton text-6xl text-purple-900">Alex</span>
@@ -9,20 +9,20 @@
                 <span class="font-hobbiton text-6xl text-purple-900">Kaili</span>
             </div>
 
-            <div class="flex items-center justify-around mt-12 font-info text-4xl">
-                <a class="bg-transparent text-purple-900 px-6 border-b border-purple-900" href="/">~ Home ~</a>
-                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900" href="/schedule">~ Schedule ~</a>
-                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900" href="/lodging">~ Lodging ~</a>
-                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900" href="/faqs">~ FAQs ~</a>
+            <div class="flex flex-wrap md:flex justify-center md:items-center md:justify-around mt-12 font-info text-xl md:text-4xl">
+                <a class="bg-transparent text-purple-900 px-6 border-b border-purple-900 mb-6 md:mb-0" href="/">~ Home ~</a>
+                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900 mb-6 md:mb-0" href="/schedule">~ Schedule ~</a>
+                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900 mb-6 md:mb-0" href="/lodging">~ Lodging ~</a>
+                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900 mb-6 md:mb-0" href="/faqs">~ FAQs ~</a>
             </div>
 
-            <div class="flex items-center justify-center max-w-sm mx-auto mt-10 mb-12">
+            <div class="flex items-center justify-center w-1/2 md:max-w-sm mx-auto mt-10 mb-12">
                 <img class="rounded-full" src="/assets/images/us2.png"/>
             </div>
 
-            <div class="text-center font-info text-5xl text-purple-900 leading-tight font-bold mt-12">
+            <div class="text-center font-info text-5xl text-purple-900 leading-tight font-bold mt-12 pb-6">
                 <div>Sunday August 10, 2019 @ 5:00pm</div>
-                <div>Chardon, OH</div>
+                <div class="mt-6">Chardon, OH</div>
             </div>
 
         </div>
--- source/schedule.blade.php
+++ source/schedule.blade.php
@@ -9,22 +9,22 @@
                 <span class="font-hobbiton text-6xl text-purple-900">Kaili</span>
             </div>
 
-            <div class="flex items-center justify-around mt-12 font-info text-4xl">
-                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900" href="/">~ Home ~</a>
-                <a class="bg-transparent text-purple-900 px-6 border-b border-purple-900" href="/schedule">~ Schedule ~</a>
-                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900" href="/lodging">~ Lodging ~</a>
-                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900" href="/faqs">~ FAQs ~</a>
+            <div class="flex flex-wrap md:flex justify-center md:items-center md:justify-around mt-12 font-info text-xl md:text-4xl">
+                <a class="bg-transparent text-purple-900 px-6 border-b border-purple-900 mb-6 md:mb-0" href="/">~ Home ~</a>
+                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900 mb-6 md:mb-0" href="/schedule">~ Schedule ~</a>
+                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900 mb-6 md:mb-0" href="/lodging">~ Lodging ~</a>
+                <a class="bg-transparent text-purple-900 px-6 border-transparent border-b hover:border-purple-900 mb-6 md:mb-0" href="/faqs">~ FAQs ~</a>
             </div>
 
-            <div class="min-w-sm max-w-4xl mx-auto font-info text-4xl text-purple-900 leading-tight mt-12">
+            <div class="w-full px-6 text-center min-w-sm max-w-4xl mx-auto font-info text-4xl text-purple-900 leading-tight mt-12">
                 <p class="font-bold mt-6 mb-6">
                     The times below are all approximate, we're not going to stress over it.
                 </p>
 
                 <div class="mx-auto flex flex-col items-center">
-                    <div class="flex flex-between justify-start pb-12 mt-6">
-                        <span class="text-4xl mr-4">5:00pm</span>
-                        <p class="ml-4 text-3xl min-w-md">
+                    <div class="flex-col md:flex flex-between justify-start pb-12 mt-6">
+                        <span class="text-4xl md:mr-4 pb-6">5:00pm</span>
+                        <p class="md:ml-4 text-3xl min-w-md">
                             A brief ceremony will be held in the backyard. We have no indoor alternate location planned, so if it calls for rain please bring an umbrella.
                         </p>
                     </div>
@@ -33,9 +33,9 @@
 
 
                 <div class="mx-auto flex flex-col items-center mt-12">
-                    <div class="flex flex-between justify-start pb-12 mt-6">
-                        <span class="text-4xl mr-4">5:30pm</span>
-                        <p class="ml-4 text-3xl min-w-md">
+                    <div class="flex-col md:flex flex-between justify-start pb-12 mt-6">
+                        <span class="text-4xl md:mr-4">5:30pm</span>
+                        <p class="md:ml-4 text-3xl min-w-md">
                             Grab a drink and find a seat. There will be a large tent area with seating as well as scattered tables. There are no assigned seats, so figure it out.
                         </p>
                     </div>
@@ -43,13 +43,12 @@
                 </div>
 
                 <div class="mx-auto flex flex-col items-center mt-12">
-                    <div class="flex flex-between justify-start pb-12 mt-6">
-                        <span class="text-4xl mr-4">6:30pm</span>
-                        <p class="ml-4 text-3xl min-w-md">
+                    <div class="flex-col md:flex flex-between justify-start pb-12 mt-6">
+                        <span class="text-4xl md:mr-4">6:30pm</span>
+                        <p class="md:ml-4 text-3xl min-w-md">
                             Dinner will be served. It is buffet style, and it will be un-organized and chaotic. Best of luck.
                         </p>
                     </div>
-                    <div class="w-full border-b border-neutral-200"></div>
                 </div>
             </div>
 
